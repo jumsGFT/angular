5e10c6437861fa801df844af713a030c971b4dd0
jums <jums@gft.com>
2025-09-25 12:37:16 +0200

    primer commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, input, Output, EventEmitter, output} from '@angular/core';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter();
+
+
+  get imagePath(): string {
+    return 'assets/users/' + this.user.avatar;
+  } 
+  
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
+}
